b6034238ae35681cc6b4494f4f82f9c79ba4e649
Marco <sonni46marcosenpai@gmail.com>
2021-08-06 01:11:19 +0200

    update

---
+++ js/main.js
@@ -0,0 +1,90 @@
+let panino = 2;
+let cheese = 1;
+let egg = 2;
+let mustard = 0.50;
+let tomato = 1;
+let lettuce = 1;
+let ketchup = 0.50;
+
+let ingredients = [panino];
+
+
+
+// document.getElementById("prezzoFinale").innerHTML = somma + " €";
+
+console.log(ingredients)
+document.getElementById("btn").addEventListener("click",
+function() {
+    let somma = 0;
+    for(let i = 0; i < ingredients.length;i++) {
+        somma += ingredients[i];
+    }
+    return document.getElementById("prezzoFinale").innerHTML = somma + " €";
+}
+);
+
+
+// function// function// function// function// function// function// function// function// function// function// function// function
+
+
+
+function yes1() {
+    ingredients.push(cheese)
+    return document.getElementById("check-1").style.display = "block";
+    }
+  
+function no1() {
+    
+    return document.getElementById("check-1").style.display = "none";
+}
+
+function yes2() {
+    ingredients.push(egg)
+    return document.getElementById("check-2").style.display = "block";
+    }
+  
+function no2() {
+    return document.getElementById("check-2").style.display = "none";
+}
+
+function yes3() {
+    ingredients.push(mustard)
+    return document.getElementById("check-3").style.display = "block";
+    }
+
+function no3() {
+    return document.getElementById("check-3").style.display = "none";
+}
+
+function yes4() {
+    ingredients.push(tomato)
+    return document.getElementById("check-4").style.display = "block";
+    }
+  
+function no4() {
+    return document.getElementById("check-4").style.display = "none";
+}
+
+function yes5() {
+    ingredients.push(lettuce)
+    return document.getElementById("check-5").style.display = "block";
+    }
+  
+function no5() {
+    return document.getElementById("check-5").style.display = "none";
+}
+
+function yes6() {
+    ingredients.push(ketchup)
+    return document.getElementById("check-6").style.display = "block";
+    }
+  
+function no6() {
+    return document.getElementById("check-6").style.display = "none";
+}
+
+
+
+
+
+
